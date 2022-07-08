be71224df121dd53de231f9475fec87c381ce5f0
nsgorlew <nsgorlew@gmail.com>
2022-07-08 07:56:59 -0700

    added airflow dag

---
+++ dags/crypto_etl_dag.py
@@ -0,0 +1,31 @@
+from datetime import timedelta
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.dates import days_ago
+
+default_args = {
+    'owner' : 'nicoadmin',
+    'depends_on_past' : False,
+    'start_date' : '2022-07-07',
+    'email' : ['nsgorlew@gmail.com'],
+    'email_on_failure' : False,
+    'email_on_retry' : False,
+    'retry_delay' : timedelta(minutes=1)
+}
+
+dag = DAG(
+    'crypto_etl_dag',
+    default_args=default_args,
+    description='CoinGecko API DAG'
+)
+
+def just_a_function():
+    print("Getting Crypto data...")
+
+run_etl = PythonOperator(
+    task_id = 'crypto__etl',
+    python_callable=just_a_function,
+    dag=dag
+)
+
+run_etl
